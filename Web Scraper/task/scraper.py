a322bd49cc16481e0d256934aa2756617fc4bc0b
radosan <radoslaw.sanetra@gmail.com>
2022-11-11 18:14:34 +0100

    st1

--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -1,8 +1,14 @@
 import requests
 
-url = input('Input the URL:\n')
-response = requests.get(url)
-if response and 'content' in response.json():
-    print(response.json()['content'])
-else:
-    print('\nInvalid quote resource!')
+
+def main():
+    url = input('Input the URL:\n')
+    response = requests.get(url)
+    if response and 'content' in response.json():
+        print(response.json()['content'])
+    else:
+        print('\nInvalid quote resource!')
+
+
+if __name__ == '__main__':
+    main()
