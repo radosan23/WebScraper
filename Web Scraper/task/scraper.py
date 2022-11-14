974ae2fcf2bac3951769cc6c62a03a9d0f1c7568
radosan <radoslaw.sanetra@gmail.com>
2022-11-14 22:11:41 +0100

    st1

--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -1,13 +1,18 @@
 import requests
+from bs4 import BeautifulSoup
 
 
 def main():
     url = input('Input the URL:\n')
-    response = requests.get(url)
-    if response and 'content' in response.json():
-        print(response.json()['content'])
+    response = requests.get(url, headers={'Accept-Language': 'en-US,en;q=0.5'})
+    if response and 'title' in response.url and 'imdb' in response.url:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        title = soup.find('h1').text
+        description = soup.find('span', {'data-testid': 'plot-l'}).text
+        movie = {'title': title, 'description': description}
+        print(movie)
     else:
-        print('\nInvalid quote resource!')
+        print('\nInvalid movie page!')
 
 
 if __name__ == '__main__':
